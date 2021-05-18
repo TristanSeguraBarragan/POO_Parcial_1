7a521f51f9c4bb2b3c53787c84e6c273f58fd19c
TF_Star_Boy <tristanfsb@outlook.com>
2021-05-17 22:20:40 -0500

    CUADRILATEROS

---
+++ src/Cuadrilatero/Rectangulo.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Cuadrilatero;
+
+/**
+ * 
+ * @author Tristán Segura
+ */
+public class Rectangulo extends Cuadrilatero{
+    protected double base, lado;
+    
+    public Rectangulo(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
+        super(x1, y1, x2, y2, x3, y3, x4, y4);
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public void setLado(double lado) {
+        this.lado = lado;
+    }
+    
+    
+    
+    
+}
